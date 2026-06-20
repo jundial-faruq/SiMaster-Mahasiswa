3ce4d78deb84fe835570009c15ce922f576505c6
jundial-faruq <jundialfaruq6@gmail.com>
2026-06-20 11:26:54 +0700

    Add InsertMhs method

--- CRUDMahasiswaADO/Form4.cs
+++ CRUDMahasiswaADO/Form4.cs
@@ -93,6 +93,42 @@ namespace CRUDMahasiswaADO
 
             return dtMahasiswa;
         }
+
+        public void InsertMhs(string nim, string nama, string alamat, string jenisKelamin,
+                      DateTime tanggalLahir, string kodeProdi, byte[] foto)
+        {
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+
+            SqlTransaction trans = conn.BeginTransaction();
+            try
+            {
+                SqlCommand command = new SqlCommand("sp_InsertMahasiswa", conn, trans);
+                command.CommandType = CommandType.StoredProcedure;
+
+                command.Parameters.AddWithValue("@pNIM", nim);
+                command.Parameters.AddWithValue("@pNama", nama);
+                command.Parameters.AddWithValue("@pAlamat", alamat);
+                command.Parameters.AddWithValue("@pTanggalLahir", tanggalLahir);
+                command.Parameters.AddWithValue("@pJenisKelamin", jenisKelamin);
+                command.Parameters.AddWithValue("@pKodeProdi", kodeProdi);
+                command.Parameters.AddWithValue("@pFoto", foto ?? (object)DBNull.Value);
+
+                command.ExecuteNonQuery();
+                trans.Commit();
+            }
+            catch (Exception ex)
+            {
+                trans.Rollback();
+                throw;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
     }
 
 }
